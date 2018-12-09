e538f297bb0e460337376553eea79b70cb9f913f
BMNTeam <maksim_bender08@mail.ru>
2018-12-09 13:42:27 +0300

    CORS added

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
-
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Accept, X-Requested-With, remember-me, Authorization');
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
