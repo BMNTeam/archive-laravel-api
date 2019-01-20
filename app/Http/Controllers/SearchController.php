dd1835da144f475db3a8edf212f299d2e2ca94a4
Maksim Barsukov <barsukov@bpcbt.com>
2019-01-20 15:11:11 +0300

    Single search result has been initialized

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Search;
 use App\Report;
 
 use Illuminate\Database\Eloquent\Collection;
@@ -13,22 +14,46 @@ class SearchController extends Controller
     {
         $search_str = $request->search;
         $reports = Report::where('name', 'like', "%$search_str%")
+            ->orWhere('short_report_text', 'like', "%$search_str%")
+            ->orWhere('full_report_text', 'like', "%$search_str%")
             ->get();
-        $reports = $this->getAsSearchResults($reports);
+        // $items = $reports->all();
+        // TODO: reorder items by rank
+//        $results = array_map(
+//            function($i, $search_str){ return new Search($i, $search_str);},
+//            $items,
+//            array_fill(0, count($items), $search_str)
+//        );
+        // $name =
+        $results = $this->getAsSearchResults($reports);
 
 
-        return new Response($reports);
+        return new Response($results);
     }
 
-    private function getAsSearchResults(Collection $reports)
+    public function getSingle(Request $request)
     {
-        return array_map(function($r) {
+        $res = null;
+        if($request->type === "report")
+        {
+            $res = Report::where('id', $request->id)->first();
+        }
+
+        return $res;
+
+
+    }
+
+    private function getAsSearchResults(Collection $reports, $type_report = true)
+    {
+        return array_map(function($r, $type_report) {
             return (object)[
                 'id' => $r->id,
                 'name' => $r->name,
                 'authors' => $r->employees,
-                'short_report_text' => $r->short_report_text
+                'type' => $type_report ? "report" : "reference",
+                'short_report_text' => substr($r->short_report_text, 0, 400)."..."
             ];
-        } ,$reports->all());
+        } ,$reports->all(), array_fill(0, count($reports->all()), $type_report));
     }
 }
