ceaf1a0787e49fc00eaaf8b12f384d571696e872
Maksim Barsukov <barsukov@bpcbt.com>
2019-02-11 09:57:59 +0300

    Search by Article and Reference has been separated

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -1,10 +1,11 @@
 <?
 namespace App\Http\Controllers;
 
+use App\Journal;
 use App\Reference;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use App\Search;
 use App\Report;
 
 use Illuminate\Database\Eloquent\Collection;
@@ -15,21 +16,12 @@ class SearchController extends Controller
     {
         $search_str = $request->search;
 
-        $reports = $this->getReportsCollection($search_str);
-        $references = $this->getReferencesCollection($search_str);
 
-        // TODO: finish with response depending on search type option
+        if($request->type === 'report') return new Response($this->getReports($search_str));
 
-        if($request->type === 'report')
-        {
-        }
+        if($request->type === 'reference')return new Response($this->getReferences($search_str));
 
-        if($request->type === 'reference')
-        {
-        }
-
-
-
+        // #region implement ranking
         // $items = $reports->all();
         // TODO: reorder items by rank
 //        $results = array_map(
@@ -38,14 +30,20 @@ class SearchController extends Controller
 //            array_fill(0, count($items), $search_str)
 //        );
         // $name =
-        $reports =$this->getAsSearchResults($reports);
-        $references = $this->getAsSearchResults($references, false);
+        // #endregion
+
+        $reports = $this->getReports($search_str);
+        $references = $this->getReferences($search_str);
+
         $results = array_merge($references, $reports);
-
-
         return new Response($results);
     }
 
+    public function download(Request $request)
+    {
+        $file = $request->query('file');
+        return response()->download(storage_path('app/'.$file));
+    }
     private function getReferencesCollection(string $search_str)
     {
         return Reference::where('name', 'like', "%$search_str%")
@@ -61,20 +59,48 @@ class SearchController extends Controller
             ->get();
     }
 
+    private function getReports(string $search_str)
+    {
+        $reports = $this->getReportsCollection($search_str);
+        return $this->getAsSearchResults($reports);
+    }
+
+    private function getReferences(string $search_str)
+    {
+        $references = $this->getReferencesCollection($search_str);
+        return $this->getAsSearchResults($references, false);
+    }
+
     public function getSingle(Request $request)
     {
         $res = null;
         if($request->type === "report")
         {
             $res = Report::where('id', $request->id)->first();
-            $res["articles"] = $res->articles()->get();
-            $res["manager"] = $res->manager()->first();
-            $res["employees"] = $res->employees()->get();
+            $articles = $res->articles()->get();
+
+            foreach ($articles as $article)
+            {
+                $article['journal']= Journal::where('id', $article->journal_id)->first();
+            }
+            $res["articles"] = $articles;
+            $res = $this->getLinkedData($res);
+            return new Response($res);
         }
 
+        if($request->type === "reference")
+        {
+            $res = Reference::where('id', $request->id)->first();
+            $res = $this->getLinkedData($res);
+            return new Response($res);
+        }
+    }
+
+    private function getLinkedData(Model $res)
+    {
+        $res["manager"] = $res->manager()->first();
+        $res["employees"] = $res->employees()->get();
         return $res;
-
-
     }
 
     private function getAsSearchResults(Collection $reports, $type_report = true)
